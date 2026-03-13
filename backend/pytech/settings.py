4eed939b421426fee06e5b41c3635b75dd5b24be
snehasivadas21 <snehasivadas2002@gmail.com>
2026-03-13 13:43:34 +0530

    Fix celery and redis url production

--- backend/pytech/settings.py
+++ backend/pytech/settings.py
@@ -236,8 +236,8 @@ MEDIA_URL = '/media/'
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
